47767b12f30a36ca602fc56a31a849105d305a6c
JafaryNm <jaffarymdegela@gmail.com>
2022-03-17 11:15:41 +0300

    Update Model

--- clientmanagement/client/models.py
+++ clientmanagement/client/models.py
@@ -1,3 +1,44 @@
+
+
 from django.db import models
 
+
 # Create your models here.
+
+class Industry(models.Model):
+   name = models.CharField(max_length = 150 , null=True)
+   
+   def __str__(self):
+       return self.name
+   
+
+class Client(models.Model):
+    name= models.CharField(max_length=150, null=True)
+    industry = models.ForeignKey(Industry, on_delete=models.CASCADE)
+    
+    
+    def __str__(self):
+        return self.name
+    
+class ServicesCategory(models.Model):
+    name=models.CharField(max_length=150, null=True)
+    
+class Project(models.Model):
+    name= models.CharField(max_length = 150)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    service_category = models.ForeignKey(ServicesCategory, on_delete=models.CASCADE)
+    date_started = models.DateField(auto_now=False, auto_now_add=False)
+    date_ended = models.DateField(auto_now=False, auto_now_add=False)
+    Price = models.FloatField()
+    
+    def __str__(self):
+        return self.name
+    
+    
+    
+    
+    
+    
+    
+    
+    
